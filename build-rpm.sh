ad0150e4aa2b934deb5b00af7a841256ec48291c
Jimmy Bergman <jimmy@atomia.com>
2012-02-08 13:46:01 +0100

    Remove old RPM artifacts from build server as well

--- build-rpm.sh
+++ build-rpm.sh
@@ -3,6 +3,8 @@
 version=`cat /etc/redhat-release | sed 's/[^0-9.]//g' | cut -d . -f 1`
 
 rm -f *rpm
+rm -f /usr/src/redhat/RPMS/*/atomiadns-*
+rm -f /usr/src/redhat/SRPMS/atomiadns-*
 
 cd dyndns
 ./buildrpms rhel"$version"
