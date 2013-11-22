7910e81f8e2172cff433fab9dcab12800729c87c
Stefan Mortensen <stefan.mortensen@atomia.com>
2013-11-22 13:49:15 +0100

    Build on Debian

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ distributor=`lsb_release -i | awk '{ print $NF }'`
 if [ -z "$distributor" ]; then
         echo "lsb_release -i failed to give distro identifier"
         exit 1
-elif [ x"$distributor" = x"Ubuntu" ]; then
+elif [ x"$distributor" = x"Ubuntu" -o x"$distributor" = x"Debian" ]; then
         ./build-apt.sh
 	exit $?
 elif [ x"$distributor" = x"RedHatEnterpriseServer" ]; then
