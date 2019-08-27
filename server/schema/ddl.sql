07057e8876288d5c615c1cf11a7615b191019556
Milica Spasic <milica.spasic@atomia.com>
2019-08-27 14:25:21 +0200

    Add possibility to add ALIAS DNS record (#51)
    * Add possibility to add ALIAS DNS record
    
    Added possibility to add ALIAS DNS record.
    
    Resolves PROD-1918. (3/3)
    
    ChangeLog:
    * [ADD] Added possibility to add ALIAS DNS records

--- server/schema/ddl.sql
+++ server/schema/ddl.sql
@@ -39,7 +39,7 @@ CREATE TABLE atomiadns_schemaversion (
 	version INT
 );
 
-INSERT INTO atomiadns_schemaversion (version) VALUES (87);
+INSERT INTO atomiadns_schemaversion (version) VALUES (89);
 
 CREATE TABLE allow_zonetransfer (
         id SERIAL PRIMARY KEY NOT NULL,
@@ -91,7 +91,8 @@ INSERT INTO allowed_type (type, synopsis, regexp) VALUES
 ('SSHFP', 'algorithm fingerprinttype fingerprint', '^[0-9]+ [0-9]+ [0-9A-F]+$'),
 ('TLSA', 'usage selector type certificate', '^[0-9]+ [0-9]+ [0-9]+ [0-9A-F]+$'),
 ('TXT', 'quotedstring', '^"[^"]{0,255}"( "[^"]{0,255}")*$'),
-('CAA', 'certificate authority authorization', '^([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5]) (issue|issuewild|iodef) ("[^"]{0,255}")*$');
+('CAA', 'certificate authority authorization', '^([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5]) (issue|issuewild|iodef) ("[^"]{0,255}")*$'),
+('ALIAS', 'hostname', '^[a-z0-9_][a-z0-9._-]*$');
 
 CREATE TABLE nameserver_group (
         id SERIAL PRIMARY KEY NOT NULL,
