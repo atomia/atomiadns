0a53a63d5b1d9fef0be7522820316c44b246e199
NVitanovic <nikola@roboten.com>
2023-03-03 10:24:02 +0100

    Remove eventlibrary as not needed

--- bind_sync/eventlibrary/afilias_sync/afilias_get_nameservers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-. /usr/share/atomiadns/eventlibrary/afilias_sync/afilias_sync.conf
-
-curl -s -k -u "$user:$pass" \
-	-H "Content-Type: text/xml" \
-	-X GET "$urlbase/nameservers/names" 2>&1 | tr "<" "\n" | grep "^name>" | cut -d ">" -f 2
-exit 0
