1485a063fb1aba6d572241cd97791cc0bf434caa
natasa-kumric <natasa.kumric@atomia.com>
2023-06-08 13:35:04 +0200

    Add support for PowerDNS 4.7 in AtomiaDNS
    Added support for PowerDNS 4.7 in AtomiaDNS.
    
    Resolves PROD-2927
    
    ChangeLog:
    
    * [FIX] Added support for PowerDNS 4.7 in AtomiaDNS.

--- powerdns_sync/schema/powerdns.sql
+++ powerdns_sync/schema/powerdns.sql
@@ -496,3 +496,9 @@ FROM (
   JOIN powerdns.global_cryptokeys c
 )
 WHERE d.type IN ('NATIVE', 'MASTER');
+
+ALTER TABLE domains ADD options VARCHAR(64000) DEFAULT NULL;
+ALTER TABLE domains ADD catalog VARCHAR(255) DEFAULT NULL;
+ALTER TABLE domains MODIFY type VARCHAR(8) NOT NULL;
+
+CREATE INDEX catalog_idx ON domains(catalog);
