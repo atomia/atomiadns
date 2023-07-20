06a57782021ee7f042211e0cf6678d3e562265a3
nemanja-zivkovic-atomia <nemanja.zivkovic@atomia.com>
2023-07-20 16:20:24 +0200

    Fix an issue due to domainmetadata_change not existing
    This issue can happen because we are still doing
    reload_updated_domainmetadata (for backward compatibility)
    and because we are not creating the table during clean install
    (as we shouldn't).
    reload_updated_domainmetadata calls GetChangedDomainIDs which reads
    from domainmetadata_change which may not exist.

--- server/schema/getchangeddomainids.sql
+++ server/schema/getchangeddomainids.sql
@@ -4,16 +4,30 @@ CREATE OR REPLACE FUNCTION GetChangedDomainIDs(
 	out change_domain_id varchar,
 	out change_changetime int
 ) RETURNS SETOF record AS $$
-DECLARE r RECORD;
+DECLARE
+	r RECORD;
+    table_exists BOOLEAN;
+    sql_query TEXT;
 BEGIN
-	FOR r IN	SELECT domainmetadata_change.id, domain_id, changetime FROM domainmetadata_change INNER JOIN nameserver ON nameserver_id = nameserver.id
-			WHERE nameserver.name = nameservername AND status = 'PENDING' ORDER BY changetime ASC, domainmetadata_change.id ASC
-	LOOP
-		change_id := r.id;
-		change_domain_id := r.domain_id;
-		change_changetime := r.changetime;
-		RETURN NEXT;
-	END LOOP;
+    SELECT EXISTS (
+        SELECT FROM information_schema.tables 
+        WHERE  table_schema = 'public'
+        AND    table_name   = 'domainmetadata_change'
+    ) INTO table_exists;
+
+    IF table_exists THEN
+        sql_query := 'SELECT domainmetadata_change.id, domain_id, changetime FROM domainmetadata_change INNER JOIN nameserver ON nameserver_id = nameserver.id WHERE nameserver.name = $1 AND status = ''PENDING'' ORDER BY changetime ASC, domainmetadata_change.id ASC';
+        
+        FOR r IN EXECUTE sql_query USING nameservername
+        LOOP
+		    change_id := r.id;
+		    change_domain_id := r.domain_id;
+		    change_changetime := r.changetime;
+		    RETURN NEXT;
+	    END LOOP;
+    ELSE
+        RAISE NOTICE 'Table domainmetadata_change does not exist.';
+    END IF;
 
 	RETURN;
 END; $$ LANGUAGE plpgsql;
